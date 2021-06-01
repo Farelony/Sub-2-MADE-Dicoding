03875cc65a59bc86352bc5412a59db759381e024
Favian Pratista <farelony@gmail.com>
2021-06-01 12:49:48 +0700

    Adding Security Section
    - Adding Obfuscation with progruad.pro
    - Adding database Encryption
    - Adding certificate pinning

--- core/src/main/java/com/example/core/data/source/remote/response/MovieResponse.kt
+++ core/src/main/java/com/example/core/data/source/remote/response/MovieResponse.kt
@@ -3,13 +3,18 @@ package com.example.core.data.source.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(
+    @SerializedName("id")
     var id : Int = 0,
     @SerializedName("backdrop_path")
     var image : String = "",
+    @SerializedName("overview")
     var overview : String = "",
     @SerializedName("poster_path")
     var poster : String = "",
+    @SerializedName("release_date")
     var release_date : String = "",
+    @SerializedName("title")
     var title : String = "",
+    @SerializedName("vote_average")
     var vote_average : Double = 0.0
 )
